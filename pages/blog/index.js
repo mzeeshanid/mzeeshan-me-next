49015161719d0ec3936b2e7508d546638a23ed8c
Muhammad Zeeshan <mzeeshanid@yahoo.com>
2021-11-29 12:17:52 +0500

    - pagination implemented

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,6 +1,15 @@
-import { LightMode, theme, Box } from "@chakra-ui/react";
+import {
+  LightMode,
+  theme,
+  Box,
+  Flex,
+  Button,
+  Spacer,
+  Center,
+} from "@chakra-ui/react";
 import { BlogJsonLd, NextSeo } from "next-seo";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 import { fetchAPI } from "../../lib/api";
 import AppFooter from "../../src/components/AppFooter";
 import AppHeadingText from "../../src/components/AppHeadingText";
@@ -9,10 +18,34 @@ import Articles from "../../src/components/Blog/Articles";
 import BlogCategories from "../../src/components/Blog/BlogCategories";
 import myNavItems from "../../src/data/myNavItems";
 
-export default function Index({ articles, categories }) {
+export default function Index({ articles, categories, totalArticles }) {
   const aTitle = "Blog - A personal blog for sharing experiences";
   const aDesc =
     "A personal blog where I write about iOS development and my personal apps. Sometime I share my travelling experiences, stories and jokes.";
+
+  const [writings, setWritings] = useState(articles);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(writings.length < totalArticles);
+  const [page, setPage] = useState(1);
+
+  const limit = 10;
+
+  const fetchArticles = async () => {
+    setIsLoading(true);
+
+    const start = page * limit;
+
+    const responseArticles = await fetchAPI(
+      `/articles?_sort=published_at:DESC&_limit=${limit}&_start=${start}`
+    );
+
+    setPage(page + 1);
+    setIsLoading(false);
+    setWritings(writings.concat(responseArticles));
+    setHasMore(writings.length + responseArticles.length < totalArticles);
+  };
+
   return (
     <LightMode>
       <NextSeo
@@ -59,7 +92,21 @@ export default function Index({ articles, categories }) {
         />
       </Box>
       <BlogCategories categories={categories} />
-      <Articles articles={articles} />
+      <Articles articles={writings} />
+      {hasMore && (
+        <Center pb={10} bg={theme.colors.gray[50]}>
+          <Button
+            colorScheme="teal"
+            variant="outline"
+            isLoading={isLoading}
+            onClick={() => {
+              fetchArticles();
+            }}
+          >
+            Load More
+          </Button>
+        </Center>
+      )}
       <AppFooter />
     </LightMode>
   );
@@ -67,13 +114,14 @@ export default function Index({ articles, categories }) {
 
 export async function getStaticProps() {
   // Run API calls in parallel
-  const [articles, categories] = await Promise.all([
-    fetchAPI("/articles?_sort=published_at:DESC"),
+  const [articles, categories, totalArticles] = await Promise.all([
+    fetchAPI("/articles?_sort=published_at:DESC&_limit=10"),
     fetchAPI("/categories"),
+    fetchAPI("/articles/count"),
   ]);
 
   return {
-    props: { articles, categories },
+    props: { articles, categories, totalArticles },
     revalidate: 1,
   };
 }
--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -8,11 +8,47 @@ import myNavItems from "../../src/data/myNavItems";
 import AppNavBar from "../../src/components/AppNavBar";
 import AppFooter from "../../src/components/AppFooter";
 import Seo from "../../src/components/Blog/BlogSEO";
+import { useState } from "react";
+import { Button } from "@chakra-ui/button";
 
-const Category = ({ category, categories, articles }) => {
+const Category = ({ category, categories, articles, totalArticles }) => {
+  // console.log(
+  //   "props --> category: ",
+  //   category,
+  //   "categories: ",
+  //   categories,
+  //   " articles: ",
+  //   articles,
+  //   "totalArticles: ",
+  //   totalArticles
+  // );
+
   const seo = {
     metaTitle: category.name,
     metaDescription: `All ${category.name} articles`,
+  };
+
+  const [writings, setWritings] = useState(articles);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(writings.length < totalArticles);
+  const [page, setPage] = useState(1);
+
+  const limit = 10;
+
+  const fetchArticles = async () => {
+    setIsLoading(true);
+
+    const start = page * limit;
+
+    const responseArticles = await fetchAPI(
+      `/articles?_sort=published_at:DESC&_limit=${limit}&_start=${start}`
+    );
+
+    setPage(page + 1);
+    setIsLoading(false);
+    setWritings(writings.concat(responseArticles));
+    setHasMore(writings.length + responseArticles.length < totalArticles);
   };
 
   return (
@@ -28,7 +64,21 @@ const Category = ({ category, categories, articles }) => {
           {category.name}
         </Heading>
       </Center>
-      <Articles articles={articles} />
+      <Articles articles={writings} />
+      {hasMore && (
+        <Center pb={10} bg={theme.colors.gray[50]}>
+          <Button
+            colorScheme="teal"
+            variant="outline"
+            isLoading={isLoading}
+            onClick={() => {
+              fetchArticles();
+            }}
+          >
+            Load More
+          </Button>
+        </Center>
+      )}
       <AppFooter />
     </LightMode>
   );
@@ -55,11 +105,15 @@ export async function getStaticProps({ params }) {
   })[0];
 
   const articles = await fetchAPI(
-    `/articles?_sort=published_at:DESC&_where[category.slug]=${params.slug}`
+    `/articles?_sort=published_at:DESC&_where[category.slug]=${params.slug}&_limit=10`
+  );
+
+  const totalArticles = await fetchAPI(
+    `/articles/count?_where[category.slug]=${params.slug}`
   );
 
   return {
-    props: { category, categories, articles },
+    props: { category, categories, articles, totalArticles },
     revalidate: 1,
   };
 }
