faa6a7d57953118eceb7420580e2037e3c8e7b47
Muhammad Zeeshan <mzeeshanid@yahoo.com>
2026-05-16 00:14:34 +0500

    Revert "- unit tests updated"
    This reverts commit b12fbdf99708cccd02f5a80d0f993f23aa8d3556.

--- vitest.config.ts
+++ vitest.config.ts
@@ -14,11 +14,10 @@ export default defineConfig({
     },
   ],
   resolve: {
-    alias: [
-      { find: /^styled-system\/(.*)/, replacement: path.resolve(__dirname, "styled-system/$1") },
-      { find: "@", replacement: path.resolve(__dirname, "src") },
-      { find: "public", replacement: path.resolve(__dirname, "public") },
-    ],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "public": path.resolve(__dirname, "public"),
+    },
   },
   test: {
     environment: "node",
