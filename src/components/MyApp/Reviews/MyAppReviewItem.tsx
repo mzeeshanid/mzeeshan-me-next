dabc420c05fd7f8ec6293f74179997c13ddaa095
Muhammad Zeeshan <mzeeshanid@yahoo.com>
2026-05-15 23:35:52 +0500

    - type error resolved

--- src/components/MyApp/Reviews/MyAppReviewItem.tsx
+++ src/components/MyApp/Reviews/MyAppReviewItem.tsx
@@ -52,7 +52,7 @@ const MyAppReviewItem: React.FC<Props> = (props: Props) => {
           <DeferredIcon
             icon={RiDoubleQuotesL}
             color={palette}
-            size={{ base: "lg", md: "xl", lg: "2xl" }}
+            size="xl"
           />
           <Text fontSize="md" color="fg.muted" mt={2}>
             {text}
